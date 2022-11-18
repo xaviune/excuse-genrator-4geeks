c614bb2899183de5ffa8217d8c28bf97eb475047
xaviune <xaviune@gmail.com>
2022-11-18 17:51:37 +0000

    working code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,46 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  function generateExcuse() {
+    const pronoun = ["A", "The"];
+    const subject = [
+      "jogger",
+      "racoon",
+      "dog",
+      "driver",
+      "comedian",
+      "pincone"
+    ];
+    const action = [
+      "took my",
+      "threw my",
+      "yelled at my",
+      "stole my",
+      "bit my"
+    ];
+
+    const possetion = ["homework", "toe", "car", "shoe"];
+    const where = ["on the street", "in my house", "in my driveway"];
+
+    const pronounIndex = Math.floor(Math.random() * pronoun.length);
+    const subjectIndex = Math.floor(Math.random() * subject.length);
+    const actionIndex = Math.floor(Math.random() * action.length);
+    const possetionIndex = Math.floor(Math.random() * possetion.length);
+    const whereIndex = Math.floor(Math.random() * where.length);
+
+    return (
+      pronoun[pronounIndex] +
+      " " +
+      subject[subjectIndex] +
+      " " +
+      action[actionIndex] +
+      " " +
+      possetion[possetionIndex] +
+      " " +
+      where[whereIndex]
+    );
+  }
+  const excuse = document.querySelector(".js-excuse");
+  excuse.innerHTML = generateExcuse();
 };
